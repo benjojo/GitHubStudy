dfa0982eca38f16e0240e650875d2c3fd9a02844
Ben Cox <ben@benjojo.co.uk>
2013-09-29 16:43:51 +0100

    Adding the starts of SQL connectivity

--- GetKeysAndParse/GetKeys.go
+++ GetKeysAndParse/GetKeys.go
@@ -3,14 +3,34 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
 )
 
+/*
+To make the table you want:
+
+CREATE TABLE `githubkeys` (
+	`keyid` INT NOT NULL,
+	`username` VARCHAR(96) NULL,
+	`key` TEXT NULL,
+	PRIMARY KEY (`keyid`),
+	INDEX `username` (`username`)
+)
+COLLATE='utf8_bin'
+ENGINE=InnoDB;
+
+*/
+
 func main() {
+	con, err := sql.Open("mysql", "root:@/random")
+	defer con.Close()
+	check(err)
 	ff, _ := os.OpenFile(os.Args[1], 0, 0)
 	f := bufio.NewReader(ff)
 	for {
@@ -49,4 +69,17 @@ func readKeys(keychain string) {
 
 func getKeySize(key string) int {
 	return 1
+}
+
+func WriteFile(path string, contents string) {
+	d1 := []byte(contents)
+	err := ioutil.WriteFile(path, d1, 0644)
+	check(err)
+}
+
+/* I have gathered that this is fairly standard. */
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
